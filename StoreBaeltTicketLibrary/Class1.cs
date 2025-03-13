8d18f10e297113749fb4f345a94bcf6e3516b454
adiliqbal96 <adiliqbal9606@gmail.com>
2025-03-13 01:56:00 +0100

    Task 8: Created a new library (StoreBaeltTicketLibrary) Copied Bridge.dll & Bridge.xml to extern dll Referenced Bridge.dll inside StoreBaeltTicketLibrary Tested that it works by creating a StoreBaeltCar class Committed and pushed to GitHub

---
+++ StoreBaeltTicketLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Bridge;
+
+namespace StoreBaeltTicketLibrary
+{
+    /// <summary>
+    /// Represents a Storebælt-specific car.
+    /// </summary>
+    public class StoreBaeltCar : Car
+    {
+        public StoreBaeltCar(string licensePlate, DateTime date)
+            : base(licensePlate, date) { }
+    }
+}
